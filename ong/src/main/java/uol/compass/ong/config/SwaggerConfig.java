b344a0514821a5b59e3f2e386bddb2ba64559565
Gabriel Coelho <gabriel.coelhox@gmail.com>
2022-04-19 00:53:29 -0300

    Adicionando funcionalidaeds do token JWT ao Swagger

--- ong/src/main/java/uol/compass/ong/config/SwaggerConfig.java
+++ ong/src/main/java/uol/compass/ong/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package uol.compass.ong.config;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,22 +11,30 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 	
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("uol.compass.ong"))
-				.paths(PathSelectors.ant("/**"))
-				.build()
-				.apiInfo(apiInfo())
-				.globalOperationParameters(Arrays.asList());
-	}
+	 @Bean
+	 public Docket api() {
+	    return new Docket(DocumentationType.SWAGGER_2)
+	            .useDefaultResponseMessages(false)
+	            .select()
+	            .apis(RequestHandlerSelectors.basePackage("uol.compass.ong"))
+	            .paths(PathSelectors.ant("/**"))
+	            .build()
+	            .securityContexts(Arrays.asList(securityContext()))
+	            .securitySchemes(Arrays.asList(apiKey()))
+	            .apiInfo(apiInfo());
+	 }
 	
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
@@ -33,4 +43,26 @@ public class SwaggerConfig {
 				.version("1.0")
 				.build();
 		}
+	
+	public ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+	
+	private SecurityContext securityContext(){
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+	
+	private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = authorizationScope;
+        SecurityReference reference = new SecurityReference("JWT", scopes);
+        List<SecurityReference> auths = new ArrayList<>();
+        auths.add(reference);
+        return auths;
+    }
 }
